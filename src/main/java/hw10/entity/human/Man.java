c2a2d6fdf9675ef3a60ddc65b752b32e8195ab0e
Ramil Huseynov <raminhuseyli@gmail.com>
2020-03-28 00:05:32 +0400

    added

---
+++ src/main/java/hw10/entity/human/Man.java
@@ -0,0 +1,39 @@
+package hw10.entity.human;
+
+import hw10.entity.DayOfWeek;
+import hw10.entity.Family;
+
+import java.text.ParseException;
+import java.util.Map;
+
+public final class Man extends Human {
+
+  public Man() {
+    super();
+  }
+
+  public Man(String name, String surname) {
+    super(name, surname);
+  }
+
+  public Man(String name, String surname, String year) throws ParseException {
+    super(name, surname, year);
+  }
+
+  public Man(String name, String surname, String year, int iq, Family family, Map<DayOfWeek, String> schedule) throws ParseException {
+    super(name, surname, year, iq, schedule, family);
+  }
+
+  public Man(String name, String surname, String year, Map<DayOfWeek, String> schedule) throws ParseException {
+    super(name, surname, year, schedule);
+  }
+
+  public void repairCar() {
+    System.out.println("Car was broken. I am repairing it");
+  }
+
+  @Override
+  public void greetPet() {
+    System.out.println("GreetPet method inside Man class");
+  }
+}
