1013f96d5c5c447db0ceb616ad4878c4dbca3cd7
Ramil Huseynov <raminhuseyli@gmail.com>
2020-03-27 23:56:43 +0400

    added

---
+++ src/main/java/hw09/Main.java
@@ -0,0 +1,102 @@
+package hw09;
+
+import hw09.controller.FamilyController;
+import hw09.entity.Family;
+import hw09.entity.human.Man;
+import hw09.entity.human.Woman;
+import hw09.entity.pet.Dog;
+import hw09.entity.pet.DomesticCat;
+import hw09.entity.pet.Pet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+  public static void main(String[] args) {
+    Pet dog = new Dog("Jim");
+    dog.setAge(15);
+    dog.setTrickLevel(25);
+    Set<String> habitsOfDog = new HashSet<>();
+    habitsOfDog.add("sleep");
+    habitsOfDog.add("eat");
+    habitsOfDog.add("play");
+    dog.setHabits(habitsOfDog);
+
+    Pet cat = new DomesticCat("Tom");
+    cat.setAge(3);
+    cat.setTrickLevel(80);
+    Set<String> habitsOfCat = new HashSet<>();
+    habitsOfCat.add("run");
+    habitsOfCat.add("play");
+    habitsOfCat.add("sleep");
+    cat.setHabits(habitsOfCat);
+
+
+    Woman motherBlack = new Woman("June", "Black", 1976);
+    motherBlack.setIq(65);
+    Man fatherBlack = new Man("Alex", "Black", 1970);
+    fatherBlack.setIq(75);
+    Family familyBlack = new Family(fatherBlack, motherBlack);
+
+
+    Man fatherAliyev = new Man("Ali", "Aliyev", 1981);
+    fatherAliyev.setIq(75);
+    Woman motherAliyev = new Woman("Nur", "Aliyev", 1983);
+    motherAliyev.setIq(65);
+    Family familyAliyev = new Family(fatherAliyev, motherAliyev);
+
+
+    Man fatherWhite = new Man("Piter", "White", 1980);
+    fatherWhite.setIq(75);
+    Woman motherWhite = new Woman("Yana", "White", 1985);
+    motherWhite.setIq(65);
+    Family familyWhite = new Family(fatherWhite, motherWhite);
+
+    FamilyController familyController = new FamilyController();
+
+    Man man1 = new Man("1111", "1111", 1999);
+    Man man2 = new Man("2222", "2222", 1998);
+    Man man3 = new Man("3333", "3333", 1997);
+    Man man4 = new Man("4444", "4444", 1996);
+    Man man5 = new Man("5555", "5555", 1995);
+    Man man6 = new Man("6666", "6666", 2008);
+    Man man7 = new Man("7777", "7777", 2005);
+    Man man8 = new Man("8888", "8888", 2010);
+
+    Man man = new Man("AAA", "BBB", 2000);
+    Woman woman = new Woman("DDD", "FFF", 2001);
+    familyController.createNewFamily(man,woman);
+
+    familyController.adoptChild(familyBlack, man1);
+    familyController.adoptChild(familyBlack, man4);
+    familyController.bornChild(familyBlack, null, "Jane");
+
+    familyController.adoptChild(familyAliyev, man2);
+    familyController.adoptChild(familyAliyev, man6);
+    familyController.adoptChild(familyAliyev, man7);
+
+    familyController.adoptChild(familyWhite, man3);
+    familyController.adoptChild(familyWhite, man5);
+    familyController.bornChild(familyWhite, "Jack", null);
+
+    familyController.saveFamily(familyBlack);
+    familyController.saveFamily(familyAliyev);
+    familyController.saveFamily(familyWhite);
+
+    familyController.addPet(2, dog);
+    familyController.addPet(3, cat);
+
+    familyController.getAllFamilies();
+    familyController.displayAllFamilies();
+    familyController.count();
+    familyController.getFamiliesBiggerThan(2);
+    familyController.getFamiliesLessThan(5);
+    familyController.countFamiliesWithMemberNumber(4);
+    familyController.deleteFamilyByIndex(1);
+    familyController.deleteFamily(familyBlack);
+    familyController.deleteAllChildrenOlderThen(17);
+    familyController.displayAllFamilies();
+    familyController.getPets(1);
+    familyController.getFamilyById(1);
+  }
+}
---
+++ src/main/java/hw09/entity/human/Human.java
@@ -0,0 +1,137 @@
+package hw09.entity.human;
+
+import hw09.entity.DayOfWeek;
+import hw09.entity.Family;
+import hw09.entity.pet.Pet;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Human {
+  private String name;
+  private String surname;
+  private int year;
+  private int iq;
+  private Family family;
+  private Map<DayOfWeek, String> schedule = new HashMap<>();
+
+
+  public Human() {
+  }
+
+  public Human(String name, String surname, int year) {
+    this(name, surname);
+    this.year = year;
+  }
+
+  public Human(String name, String surname, int year, Map<DayOfWeek, String> schedule) {
+    this(name, surname, year);
+    this.schedule = schedule;
+  }
+
+  public Human(String name, String surname, int year, int iq, Map<DayOfWeek, String> schedule, Family family) {
+    this(name, surname, year, schedule);
+    this.iq = iq;
+    this.family = family;
+  }
+
+  public Human(String name, String surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  public void greetPet() {
+    System.out.printf("Hello, %s", family.getPet()
+            .stream().map(Pet::getNickname).collect(Collectors.toList()).toString());
+  }
+
+  public void describePet() {
+    if (family == null || family.getPet() == null) System.out.println("There is no any pets.");
+    else {
+      for (Pet pets : family.getPet()) {
+        System.out.printf("I have a %s, he is %d years old, he is %s.\n",
+                pets.getSpecies(), pets.getAge(), pets.getTrickLevel() > 49 ? "very sly" : "almost not sly");
+      }
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public int getIq() {
+    return iq;
+  }
+
+  public void setIq(int iq) {
+    this.iq = iq;
+  }
+
+  public Map<DayOfWeek, String> getSchedule() {
+    return schedule;
+  }
+
+  public void setSchedule(Map<DayOfWeek, String> schedule) {
+    this.schedule = schedule;
+  }
+
+  public Family getFamily() {
+    return family;
+  }
+
+  public void setFamily(Family family) {
+    this.family = family;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Human[name='%s', surname='%s', year=%d, iq=%d, schedule=%s]\n",
+            name, surname, year, iq, schedule.toString());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Human human = (Human) o;
+    return getYear() == human.getYear() &&
+            getIq() == human.getIq() &&
+            Objects.equals(getName(), human.getName()) &&
+            Objects.equals(getSurname(), human.getSurname()) &&
+            Objects.equals(getFamily(), human.getFamily()) &&
+            Objects.equals(getSchedule(), human.getSchedule());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName(), getSurname(), getYear(), getIq(), getFamily(), getSchedule());
+  }
+
+  @Override
+  protected void finalize() throws Throwable {
+    System.out.println("Object of Human class deleted");
+    super.finalize();
+  }
+}
