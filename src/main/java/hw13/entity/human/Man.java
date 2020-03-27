dc44fa7cb9fce0dee0f41ba85d5a20bcc98f8618
Ramil Huseynov <raminhuseyli@gmail.com>
2020-03-28 00:08:30 +0400

    added

---
+++ src/main/java/hw13/entity/human/Man.java
@@ -0,0 +1,43 @@
+package hw13.entity.human;
+
+import hw13.entity.DayOfWeek;
+import hw13.entity.Family;
+
+import java.text.ParseException;
+import java.util.Map;
+
+public final class Man extends Human {
+
+  public Man() {
+    super();
+  }
+
+  public Man(String name, String surname) {
+    super(name, surname);
+  }
+
+  public Man(String name, String surname, String year) throws ParseException {
+    super(name, surname, year);
+  }
+
+  public Man(String name, String surname, String year, int iq) throws ParseException {
+    super(name, surname, year, iq);
+  }
+
+  public Man(String name, String surname, String year, int iq, Family family, Map<DayOfWeek, String> schedule) throws ParseException {
+    super(name, surname, year, iq, schedule, family);
+  }
+
+  public Man(String name, String surname, String year, Map<DayOfWeek, String> schedule) throws ParseException {
+    super(name, surname, year, schedule);
+  }
+
+  public void repairCar() {
+    System.out.println("Car was broken. I am repairing it");
+  }
+
+  @Override
+  public void greetPet() {
+    System.out.println("GreetPet method inside Man class");
+  }
+}
---
+++ src/main/java/hw13/entity/pet/Species.java
@@ -0,0 +1,9 @@
+package hw13.entity.pet;
+
+public enum Species {
+  FISH,
+  ROBOCAT,
+  DOG,
+  DOMESTICCAT,
+  UNKNOWN
+}
