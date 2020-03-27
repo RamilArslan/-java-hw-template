4133a8d5b509b2184c29c18d51976426f07abf5f
Ramil Huseynov <raminhuseyli@gmail.com>
2020-03-27 13:25:52 +0400

    added

---
+++ src/main/java/hw11/entity/pet/RoboCat.java
@@ -0,0 +1,30 @@
+package hw11.entity.pet;
+
+import java.util.Set;
+
+public class RoboCat extends Pet {
+  RoboCat() {
+    super();
+  }
+
+  RoboCat(String nickname) {
+    super(nickname);
+    species = Species.ROBOCAT;
+
+  }
+
+  RoboCat(String nickname, int age, int trickLevel, Set<String> habits) {
+    super(nickname, age, trickLevel, habits);
+    species = Species.ROBOCAT;
+  }
+
+  @Override
+  public void respond() {
+    System.out.println("I am a little robot");
+  }
+
+  @Override
+  public void foul() {
+    System.out.println(".........");
+  }
+}
